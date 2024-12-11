8b77525bd8145ee8e4bbf62e95c897b99de2b48d
86189 <chenhai0731@gmail.com>
2024-12-11 22:42:34 +0800

    Remove Config Setting Remove all functions except for anti recall

--- app/src/main/java/nep/timeline/re_telegram/HookInit.java
+++ app/src/main/java/nep/timeline/re_telegram/HookInit.java
@@ -9,17 +9,10 @@ import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import nep.timeline.re_telegram.application.ApplicationLoaderHook;
-import nep.timeline.re_telegram.features.AllowMoveAllChatFolder;
-import nep.timeline.re_telegram.features.AntiAntiForward;
-import nep.timeline.re_telegram.features.AntiRecallWithDatabase;
-import nep.timeline.re_telegram.features.HideStories;
 import nep.timeline.re_telegram.features.NEWAntiRecall;
-import nep.timeline.re_telegram.features.NoSponsoredMessages;
-import nep.timeline.re_telegram.features.ProhibitChannelSwitching;
-import nep.timeline.re_telegram.features.UseSystemTypeface;
 
 public class HookInit implements IXposedHookLoadPackage {
-    private static final List<String> hookPackages = Arrays.asList("org.telegram.messenger", "org.telegram.messenger.web", "org.telegram.messenger.beta", "org.telegram.plus", "org.telegram.mdgram",
+    private static final List<String> hookPackages = Arrays.asList("org.telegram.messenger", "org.telegram.messenger.web", "org.telegram.messenger.beta", "org.telegram.plus",
             "tw.nekomimi.nekogram",
             "com.cool2645.nekolite",
             "com.exteragram.messenger",
@@ -62,26 +55,15 @@ public class HookInit implements IXposedHookLoadPackage {
 
             ApplicationLoaderHook.init(classLoader);
 
-            if (ClientChecker.check(ClientChecker.ClientType.Yukigram)) {
-                AntiRecallWithDatabase.initUI(classLoader);
-
-                AntiRecallWithDatabase.initProcessing(classLoader);
-
-                AntiRecallWithDatabase.init(classLoader);
-            } else {
                 NEWAntiRecall.initUI(classLoader);
 
                 NEWAntiRecall.initProcessing(classLoader);
 
                 NEWAntiRecall.init(classLoader);
-            }
 
-            // FakePremium.init(classLoader);
 
-            if (!ClientChecker.check(ClientChecker.ClientType.Nekogram))
-                AllowMoveAllChatFolder.init(classLoader);
+            //AllowMoveAllChatFolder.init(classLoader);
 
-            // UnlockedNoPremiumAccountsLimit.init(classLoader);
 
             if (ClientChecker.check(ClientChecker.ClientType.Cherrygram)) {
                 Class<?> checker = XposedHelpers.findClassIfExists("org.telegram.tgnet.ConnectionsManagerImpl", classLoader);
@@ -89,7 +71,8 @@ public class HookInit implements IXposedHookLoadPackage {
                 XposedHelpers.findAndHookMethod(checker, "g45ytgt513", XC_MethodReplacement.DO_NOTHING);
             }
 
-            if (!onlyNeedAR(lpparam.packageName))
+/*
+          if (!onlyNeedAR(lpparam.packageName))
             {
                 if (!ClientChecker.check(ClientChecker.ClientType.MDgram))
                     UseSystemTypeface.init(classLoader);
@@ -106,6 +89,8 @@ public class HookInit implements IXposedHookLoadPackage {
 
                 AntiAntiForward.init(classLoader);
             }
+
+ */
         }
     }
 }
